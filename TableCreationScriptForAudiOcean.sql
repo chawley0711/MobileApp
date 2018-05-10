9c94d8a9c6f2028b36ede5375648375c39993c77
Jeffrey <jayhenderson758@gmail.com>
2018-05-09 22:47:16 -0600

    Added table creation script for the sql server database

---
+++ TableCreationScriptForAudiOcean.sql
@@ -0,0 +1,35 @@
+
+Create Table Users(ID int Primary Key identity(1,1)
+					, [Name] varchar(20)
+					, DisplayName varchar(20)
+					, ProfilePictureURL varchar(max))
+go
+
+Create Table Genres(ID int Primary Key identity(1,1), [Name] varchar(20))
+
+go
+
+Create Table Songs(ID int Primary Key identity(1,1),
+					SongName varchar(30),
+					OwnerID int Foreign Key references Users(ID), 
+					DateUploaded timestamp, 
+					GenreID int foreign key references Genres(ID))
+go
+
+Create Table Ratings(UserID int Foreign Key References Users(ID)
+					,SongID int Foreign Key References Songs(ID)
+					,Rating int,
+					Primary Key(UserID, SongID))
+go
+
+Create Table Subscriptions(SubscriberID int Foreign Key References Users(ID)
+							,UserID int Foreign Key References Users(ID),
+							Primary Key(SubscriberID, UserID))
+go
+
+Create Table Comments(CommentID int Primary Key identity(1,1)
+						,SongID int foreign Key References Songs(ID)
+						,[Text] varchar(max)
+						,DatePosted timestamp
+						,UserID int Foreign Key References Users(ID))
+go
