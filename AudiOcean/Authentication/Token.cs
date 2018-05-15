5e9dd47d13f4bebdb22a3bb609364458cd985b95
Tyeboe <jayhenderson758@gmail.com>
2018-05-15 10:21:42 -0600

    Added the Token abstract class and ITokenRetriever interface to new Authentication project

---
+++ AudiOcean/Authentication/Token.cs
@@ -0,0 +1,14 @@
+﻿namespace AudiOcean.Droid
+{
+    public class Token<T, U>
+    {
+        T Value { get; }
+
+        U Payload { get; set; }
+
+        public Token(T token)
+        {
+            this.Value = token;
+        }
+    }
+}
