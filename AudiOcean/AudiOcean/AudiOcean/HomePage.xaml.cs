55ac35ac0c9440d80a43a9e9aabdf2699da86741
Unknown <jordon.bowden@gmail.com>
2018-05-15 19:02:49 -0600

    Got project working by changing properties, and moved image to drawable

--- AudiOcean/AudiOcean/AudiOcean/HomePage.xaml.cs
+++ AudiOcean/AudiOcean/AudiOcean/HomePage.xaml.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace GUILibrary
+namespace AudiOcean
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class HomePage : ContentPage
