3766c4d50622ac815b6c79e97adeb2c18dc23794
Tyeboe <jayhenderson758@gmail.com>
2018-05-16 18:10:40 -0600

    Closes #5

--- AudiOcean/GoogleAuthentication/GoogleTokenValidator.cs
+++ AudiOcean/GoogleAuthentication/GoogleTokenValidator.cs
@@ -8,7 +8,6 @@ using System.Threading.Tasks;
 using static Google.Apis.Auth.GoogleJsonWebSignature;
 using Google.Apis.Auth.OAuth2;
 
-
 namespace GoogleAuthentication
 {
     public class GoogleTokenValidator : ITokenValidator<string, Payload>
@@ -18,7 +17,6 @@ namespace GoogleAuthentication
             var validationTask = ValidateAsync(token.Value);
             validationTask.RunSynchronously();
             token.Payload = validationTask.Result;
-
             return token.Payload != null;
         }
     }
